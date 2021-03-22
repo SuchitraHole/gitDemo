d7f4d1cb0aa11c931ba1b02ac4146a1b8d6ae01f
SuchitraHole <prachi.hole98@gmail.com>
2021-03-22 18:10:56 +0530

    second comit

--- SuchitraGit/src/hello/Calculator.java
+++ SuchitraGit/src/hello/Calculator.java
@@ -1,10 +1,14 @@
 package hello;
 
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Add;
+
 public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hrllo");
+		
+System.out.println(2+3);		
 	}
 
 }
