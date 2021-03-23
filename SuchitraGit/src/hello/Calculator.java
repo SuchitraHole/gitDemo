60247eca87e40442641d184dd43d41310f5650e1
SuchitraHole <prachi.hole98@gmail.com>
2021-03-23 09:59:35 +0530

    added sq and sq2 in br2

--- SuchitraGit/src/hello/Calculator.java
+++ SuchitraGit/src/hello/Calculator.java
@@ -14,4 +14,7 @@ System.out.println(2+3);
 public static void sq(int num1, int num2) {
 	System.out.println(num1*num2);
 }
+public static void sq2(int num1, int num2) {
+	System.out.println(num1*num2);
+}
 }
